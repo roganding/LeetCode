250021d36830ad6b32a1dc5a137ad66ff43b91cd
R0gan <dingshuyong@gmail.com>
2021-07-11 17:21:45 +0800

    fix No176.sql 简化SQL，调整排版

--- sql/No176.sql
+++ sql/No176.sql
@@ -1,11 +1,16 @@
 -- 编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary）
-WITH salarys(id, salary)
-AS (SELECT row_number() over (ORDER BY salary DESC) AS id, Salary AS salary FROM employee GROUP BY Salary)
+WITH salarys(id, salary) AS (
+    SELECT row_number() over (ORDER BY salary DESC) AS id,
+    Salary AS salary
+    FROM employee
+    GROUP BY Salary
+)
 
 SELECT
-case COUNT(1)
-	when 0 THEN NULL
-	ELSE max(t.salary) END AS SecondHighestSalary
-FROM (
-	SELECT s.salary FROM salarys s WHERE s.id = 2
-)  t
+    case COUNT(1)
+        when 0 THEN NULL
+        ELSE t.salary
+	END
+	AS SecondHighestSalary
+FROM salarys t
+WHERE t.id = 2
