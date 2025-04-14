3969a4e0b87907edba1a35dbc5d2913746645a57
dingsy <dingshuyong@gmail.com>
2025-04-14 18:18:51 +0800

    Mid Z字形变换

---
+++ algorithm/java/leetcode06/Solution.java
@@ -0,0 +1,24 @@
+package leetcode06;
+
+/**
+ * Z 字形变换
+ * todo
+ * @author rogan
+ * @date 2024/12/23
+ */
+public class Solution {
+
+    public static String convert(String s, int numRows) {
+        if (s == null) {
+            return null;
+        }
+        int length = s.length();
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
